e0badf704f7563a73151617d6578acb4802328e3
mistachkin <mistachkin@noemail.net>
2021-12-06 22:43:22 +0000

    Add a missing diagnostic message.
    FossilOrigin-Name: 5f0696d3bab165e5a0a1d4c46dc209e2c93fe1f0

--- System.Data.SQLite/SQLiteConnectionPool.cs
+++ System.Data.SQLite/SQLiteConnectionPool.cs
@@ -1,7 +1,7 @@
 /********************************************************
  * ADO.NET 2.0 Data Provider for SQLite Version 3.X
  * Written by Robert Simpson (robert@blackcastlesoft.com)
- * 
+ *
  * Released to the public domain, use at your own risk!
  ********************************************************/
 
@@ -420,7 +420,11 @@ namespace System.Data.SQLite
 
         public void ResetCounts()
         {
-
+            if (log != null)
+            {
+                log.AppendFormat(
+                    "ResetCounts(){0}", Environment.NewLine);
+            }
         }
         #endregion
 
